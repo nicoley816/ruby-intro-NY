b64ad7c4ee85d97f17d5d1ca658974868b0f2531
Nicole Yu <nicole.yu@kellogg.northwestern.edu>
2025-04-22 14:49:27 +0000

    Initial lecture and lab 2 answers

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,28 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+puts 5
+puts 3+5
+puts 3/5.0
 
 # Non-numbers
+puts "unicorn"
 
 # True, False
 
 # Nothing
 
 # Variables
-
+amount = 3
+y = 5
+food = "Tacos"
+puts amount
+puts y
+z = 3 * 5
+z = 10
+puts z
 # Combine strings and variables
-
+puts "#{food} on todays menu: #{amount}"
 # String manipulation
+puts food.upcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,41 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+true
+false
 
 # Boolean Expressions
+x = 3
+y = 5
+# puts x == y
+# puts x != y
+# puts x > y
+# puts x < y
+# puts x >= y
+# puts x <= y
 
 # If Conditional Logic
+# if x == y
+#     puts "what???"
+#     #end
+# if x != y
+#     puts "Not the same"
+# puts "down here"
+# end
 
 # If/Else Conditional Logic
+# if x == y
+#     puts "what????"
+# else
+#     puts "whew, math works"
 
 # Elsif Conditional Logic
+if x == y || x == 10
+    puts "What???"
+elsif x > y
+    puts "this shouldn't work"
+else
+    puts "phew...math works"
 
 # Combining Expressions
+end
